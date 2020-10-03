4cafd6b8002f71cef26ccf7b3b7d719f34e2d13c
joseph <josephjohndemarco@gmail.com>
2020-10-03 16:44:20 -0400

    Add props to constructor and super.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import Calculator from './components/Calculator'
 
 class App extends Component{
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       total: '',
